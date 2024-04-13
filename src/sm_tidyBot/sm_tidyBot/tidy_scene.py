44a681ab121ff57fd7afa0556b96d09e98c0f15b
25730188 <25730188@students.lincoln.ac.uk>
2024-04-13 16:44:36 +0000

    contour highlighting working.

--- src/sm_tidyBot/sm_tidyBot/tidy_scene.py
+++ src/sm_tidyBot/sm_tidyBot/tidy_scene.py
@@ -21,14 +21,31 @@ class TidyScene(Node):
         self.cvBridge = CvBridge()
 
     def onTick(self):
-        print("")
+        bamting = True # Haven't implemented Yet.
 
     def cameraCallback(self, data):
         # Convert ROS Image message to OpenCV image
-        currentFrame = self.cvBridge.imgmsg_to_cv2(data, desired_encoding = "bgr8")
+        frame = self.cvBridge.imgmsg_to_cv2(data, desired_encoding = "bgr8")
+
+        # Convert image to HSV
+        frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        
+        # Create mask for range of colours (HSV low values, HSV high values)
+        frameMask = cv2.inRange(frameHSV,(0, 150, 50), (255, 255, 255)) # orange
+
+        contours, hierarchy = cv2.findContours(frameMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Sort by area (keep only the biggest one)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
+
+        # Draw contour(s) (image to draw on, contours, contour number -1 to draw all contours, colour, thickness):
+        frameContours = cv2.drawContours(frame, contours, 0, (255, 255, 0), 20)
+
+        # show the cv images
+        frameContoursSmall = cv2.resize(frameContours, (0,0), fx=0.4, fy=0.4) # reduce image size
 
         # Show the image.
-        cv2.imshow("Image Window",currentFrame)
+        cv2.imshow("Image Window", frameContoursSmall)
         cv2.waitKey(1)
 
 
@@ -53,3 +70,16 @@ def main(args = None):
 
 if __name__ == '__main__':
     main()
+
+# ROBOT PLAN
+# 
+# Need robot to find cubes, and push them to any wall.
+# Use LIDAR & Camera Feed to determine if a cube is at the wall or not.
+#
+#
+#
+#
+#
+#
+#
+#
