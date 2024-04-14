d09bc44f68c889f6e38b94e52d2ae5a0f65df382
25730188 <25730188@students.lincoln.ac.uk>
2024-04-14 12:25:47 +0000

    adding laser scan

--- src/sm_tidyBot/sm_tidyBot/tidy_scene.py
+++ src/sm_tidyBot/sm_tidyBot/tidy_scene.py
@@ -2,19 +2,27 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2
 import numpy as np
 
 class TidyScene(Node):
-    def __init__(self):
+    def __init__(self, deltaTime):
         super().__init__('TidyScene')
 
         self.targetObject = None
+        self.dt = deltaTime
+        self.cubeSize = 0.2
+        self.distance = 0
 
-        # Subscribe to the camera topic.
-        self.create_subscription(Image, "/limo/depth_camera_link/image_raw", self.cameraCallback, 10)
+        # Subscribe to the camera and laser topic.
+        self.create_subscription(Image, "/limo/depth_camera_link/image_raw", self.cameraCallback, 1 / deltaTime)
+        self.create_subscription(LaserScan, "/scan", self.laserScanCallback, 1 / deltaTime)
+
+        # Create a timer
+        self.timer = self.create_timer(deltaTime, self.onTick)
 
         # Create Bridge object. Used to convert between ROS2 and OpenCV Images.
         self.cvBridge = CvBridge()
@@ -29,8 +37,8 @@ class TidyScene(Node):
         # Create mask for range of colours (HSV low values, HSV high values)
         frameMask = cv2.inRange(frameHSV,(0, 150, 50), (255, 255, 255)) # orange
 
-        # Find all contours in view.
-        contours = cv2.findContours(frameMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # Find all Contours
+        contours, hierarchy = cv2.findContours(frameMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         # Draw contour(s) (image to draw on, contours, contour number -1 to draw all contours, colour, thickness):
         frameContours = cv2.drawContours(frame, contours, -1, (255, 255, 0), 20)
@@ -46,7 +54,7 @@ class TidyScene(Node):
         self.targetObject = None
 
         # Find our target object
-        self.FindTargetObject(contours, data)
+        self.findTargetObject(contours, data)
 
         if not self.targetObject:
             print("No target found")
@@ -60,25 +68,34 @@ class TidyScene(Node):
 
         # If the object's center is to the left, turn left.
         if cx < data.width / 3:
-            print("I wanna turn left")
+            #print("I wanna turn left")
+            bamting = False
         # If the object's center is to the right, turn right.
         elif cx >= 2 * data.width / 3:
-            print("I wanna turn right")
+            #print("I wanna turn right")
+            bamting = False
         # The object is within 100px of the camera's center.
         else: 
-            print("Im looking at my target")
+            #print("Im looking at my target")
+            bamting = True
 
+    def laserScanCallback(self, data):
+        bamting = True
 
-    def FindTargetObject(self, contours, data):
+    def onTick(self):
+        bamting = True
+
+    def findTargetObject(self, contours, data):
+        currentContour = 0
         # If we can see a contour, rotate towards it
-        print(contours)
         for contour in contours:
-            
+            print("Checking validity of contour ", currentContour)
+            currentContour += 1
             M = cv2.moments(contour)
             if M['m00'] <= 0:
                 continue
             
-            # Get the Height of the contour
+            # Get the Height of the contour center
             cy = int(M['m01']/M['m00'])
 
             # If the object is higher than our robot. Its probably out of reach. Ignore it.
@@ -87,19 +104,31 @@ class TidyScene(Node):
                 continue
 
             # TODO: Check if this box is against a wall or not. LIDAR is your best bet.
+            self.calculateDistance(contour, data)
+            print("Object distance = ", self.distance)
 
             # If we got here, We want to head to this cube.
             self.targetObject = M
-            print("Found target Object")
+            #print("Found target Object")
             break
+
+    def calculateDistance(self, contour, data):
+        # Get the bounding box of this contour.
+        # Then get the width of it. We can use this to calculate the distance.
+        # https://www.geeksforgeeks.org/realtime-distance-estimation-using-opencv-python/
+        boundingBox = cv2.minAreaRect(contour)
+        apparentWidth = boundingBox[1][0]
+        self.distance = (self.cubeSize * data.width / apparentWidth)
 
 def main(args = None):
 
     # Initialize ROS2. Must be done before nodes are created.
     rclpy.init(args = args)
 
+    deltaTime = 1 / 10
+
     # Create Instance of our TidyScene Program.
-    tidyScene = TidyScene()
+    tidyScene = TidyScene(deltaTime)
 
     # Executes work on the node until its shut down.
     rclpy.spin(tidyScene)
