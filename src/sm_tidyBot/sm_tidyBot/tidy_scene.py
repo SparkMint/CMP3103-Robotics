08e11bb3b9bce0fc5e04e88cc3bcc33f9dcb3573
25730188 <25730188@students.lincoln.ac.uk>
2024-04-16 14:01:05 +0000

    Robot using depth sensor now!

--- src/sm_tidyBot/sm_tidyBot/tidy_scene.py
+++ src/sm_tidyBot/sm_tidyBot/tidy_scene.py
@@ -24,25 +24,33 @@ class TidyScene(Node):
 
         # Extra Variables
         self.robotState = RobotState.LookForTarget
-        self.targetObject = None
-        self.cubeSize = .05
+
+        self.targetObjectIndex = 0
         self.distanceFromTarget = 0
+        self.pushForwardTime = 0
+        self.backUpTime = 0
 
         # Twist Variables
-        self.angularVelocity = 0
-        self.linearVelocity = 0
+        self.angularVelocity = 0.0
+        self.linearVelocity = 0.0
         self.twist = None
 
         # Camera Variables
-        self.contours = None
+        self.contourFrame = None
+        self.depthFrame = None
         self.cameraData = None
+
+        self.contours = None
+        self.targetContour = None
+        self.targetContourX = 0
+        self.targetContourY = 0
 
         # Laser Variables
         self.laserData = None
-        self.laserAngleDeg = 0
 
         # Subscribe to the camera and laser topic.
         self.create_subscription(Image, "/limo/depth_camera_link/image_raw", self.cameraCallback, sampleRate)
+        self.create_subscription(Image, "/limo/depth_camera_link/depth/image_raw", self.depthCallback, sampleRate)
         self.create_subscription(LaserScan, "/scan", self.laserScanCallback, sampleRate)
 
         # Create Publishers.
@@ -59,165 +67,183 @@ class TidyScene(Node):
         # Store our camera's data    
         self.cameraData = cameraData
 
-        # Convert ROS Image message to OpenCV image
-        frame = self.cvBridge.imgmsg_to_cv2(cameraData, desired_encoding = "bgr8")
+        # Convert ROS Image message to an OpenCV RGB image
+        self.contourFrame = self.cvBridge.imgmsg_to_cv2(cameraData, "bgr8")
 
         # Convert image to HSV
-        frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        
         # Create mask for range of colours (HSV low values, HSV high values)
-        frameMask = cv2.inRange(frameHSV,(0, 150, 50), (255, 255, 255)) # orange
-
         # Find all Contours
+        frameHSV = cv2.cvtColor(self.contourFrame, cv2.COLOR_BGR2HSV)
+        frameMask = cv2.inRange(frameHSV,(0, 150, 50), (255, 255, 255))
         self.contours, hierarchy = cv2.findContours(frameMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Draw all contours we can see.
-        # TODO: Draw the target we find with a different colour.
-        frameContours = cv2.drawContours(frame, self.contours, -1, (255, 255, 0), 20)
-
-        # show the cv images
-        frameContoursSmall = cv2.resize(frameContours, (0,0), fx=0.4, fy=0.4) # reduce image size
-
-        # Show the image.
-        cv2.imshow("Image Window", frameContoursSmall)
+        # Draw our contours onto the frame and reduce the size
+        if self.targetObjectIndex <= len(self.contours) - 1:      
+            frameContours = cv2.drawContours(self.contourFrame, self.contours, self.targetObjectIndex, (255, 255, 0), 20)
+            frameContoursSmall = cv2.resize(frameContours, (0,0), fx=0.4, fy=0.4)
+            cv2.imshow("Image Window", frameContoursSmall)
+        else:
+            noContourFrameSmall = cv2.resize(self.contourFrame, (0,0), fx=0.4, fy=0.4)
+            cv2.imshow("Image Window", noContourFrameSmall)
+            
         cv2.waitKey(1)
+
+    def depthCallback(self, depthData):
+        # Convert ROS Image message to an OpenCV Depth frame.
+        self.depthFrame = self.cvBridge.imgmsg_to_cv2(depthData, "32FC1")
 
     def laserScanCallback(self, laserData):
         self.laserData = laserData
 
     def onTick(self):
         # Both data types need to be present to decide what to do.
-        if self.cameraData is None or self.laserData is None:
+        if self.cameraData is None or self.laserData is None or self.depthFrame is None:
             return
         
-        # Refresh Velocities each tick.
-        self.angularVelocity = float(0)
-        self.linearVelocity = float(0)
-
-        print("Robot State: ", self.robotState.name)
+        # Look for suitable target objects.
+        self.selectTargetContour()
 
         # Look For Target State
+        #
+        # In this state, we react to the data provided by the target contour selection function found above.
+        # If there is a target, we want to head to it and line ourselves up to it. If not, we want to look around
+        # so the target contour selection function can run each perceived object through it.
         if self.robotState is RobotState.LookForTarget:
-            # Reset our target for this tick.
-            self.targetObject = None
-
-            # Find our target object
-            self.findTargetObject()
-
-            # Check if we have a target to turn towards
-            if self.targetObject is None:
-                self.angularVelocity = 0.75
-            else:
-                self.headToTarget()
-                # If we get here. We want to start rotating towards the target.
-                # When it is lined up, it will automatically switch the robot's state to Pushing
+            self.lookForTargetBehavior()
   
         # Push Target State
+        #        
+        # Since the robot loses vision of the cube when pushing it, we want to check every now and again that
+        # we haven't lost the cube. So we apply a "Two Steps forward, one step back" approach. Where the robot will
+        # Push the cube forward, then move back a bit and check that they haven't lost the cube.
+        # We keep performing this until we get close enough to the wall.
         if self.robotState is RobotState.PushTarget:
-            self.linearVelocity = 0.2
-            self.angularVelocity = 0.0
+            self.pushBehavior()
 
-            forwardLaserDistance = self.laserData.ranges[int(len(self.laserData.ranges) / 2)]
-
-            if forwardLaserDistance < 0.25:
-                self.robotState = RobotState.BackUpFromTarget
-
-        # Move Away State
+        # Back up State
+        #
+        # This is our "One step back" state. After doing this, we want to return to looking for a valid target.
+        # If the cube isn't against the wall, the robot will see it and return to pushing.
         if self.robotState is RobotState.BackUpFromTarget:
-            self.linearVelocity = -0.5
-            self.angularVelocity = 0.0
+            self.backUpBehavior()
 
-            forwardLaserDistance = self.laserData.ranges[int(len(self.laserData.ranges) / 2)]
+        # Drive the robot
+        self.driveRobot()
 
-            if forwardLaserDistance > 0.3:
-                self.robotState = RobotState.LookForTarget
 
+    # BEHAVIOR FUNCTIONS
+    def selectTargetContour(self):
+        # Reset our target for this tick.
+        self.targetContour = None
+        self.targetContourX = 0
+        self.targetContourY = 0
+
+        # Sort by area. The biggest (Closest ones) should take priority
+        self.contours = sorted(self.contours, key=cv2.contourArea, reverse=True)
+
+        # If we can see a contour, rotate towards it
+        for cIndex, contour in enumerate(self.contours):
+            M = cv2.moments(contour)
+            if M['m00'] <= 0:
+                continue
+            
+            # Get the Width and Height of the contour
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+
+            # If the object is higher than our robot. Its probably out of reach. Ignore it.
+            if cy <= self.cameraData.height / 3:
+                continue
+
+            # Get the distance to our object as well as the angle required to get to it.
+            distanceFromContour = self.depthFrame[cy][cx]
+
+            # Find the angle in degrees using the horizontal center of the
+            # chosen contour, the centre of the camera and the FOV (Seems to be 90 degrees)
+            # https://github.com/IntelRealSense/librealsense/issues/5553
+            laserAngleDeg = int(((cx - self.cameraData.width / 2) / self.cameraData.width / 2) * (90 / 2))
+
+            # Determine the laser segment we want to get our wall distance reading from.
+            # It should be in the direction of the cube we are targeting.
+            # We can then get a distance between the object and the wall.
+            # TODO: Update this to use a small range
+            wallDistance = self.laserData.ranges[int(len(self.laserData.ranges) / 2) - laserAngleDeg]
+            objectToWallDistance = wallDistance - distanceFromContour
+
+            # Check if the distance between the object and the wall is less than the specified
+            # amount. If it is. We have already pushed that to the wall. Ignore it!
+            if objectToWallDistance < 0.:
+                continue
+
+            # Changes the colour of the target laser in RVIZ2. Used for debug purposes.
+            self.laserData.intensities[int(len(self.laserData.ranges) / 2) - laserAngleDeg] = 100
+            self.publishLaserScan.publish(self.laserData)
+
+            # If we got here. We have found a valid target. Select this one and return.
+            self.targetObjectIndex = cIndex
+            self.targetContour = contour
+            self.distanceFromTarget = distanceFromContour
+            self.targetContourX = cx
+            self.targetContourY = cy
+
+            print("INFO")
+            print("Object Distance = ", self.distanceFromTarget)
+            print("Wall Distance = ", wallDistance)
+            print("Object to Wall Distance = ", objectToWallDistance)
+
+            return
+        
+        self.targetObjectIndex = 999
+
+    def lookForTargetBehavior(self):
+        print("Target Dist:", self.distanceFromTarget)
+        # Check if we have a target to turn towards
+        if self.targetContour is None:     
+            self.angularVelocity = 0.75
+        else:
+            if self.distanceFromTarget > 0.5: # Move Towards our target
+                self.linearVelocity = 0.2
+
+            # If the object's center is to the left, turn left.
+            if self.targetContourX < self.cameraData.width / 2.25:
+                self.angularVelocity = 0.2
+
+            # If the object's center is to the right, turn right.
+            elif self.targetContourX > 2 * self.cameraData.width / 2.25:
+                self.angularVelocity = -0.2
+
+            # The object is in the center of our camera's view. Switch to the push state.
+            elif self.distanceFromTarget <= 0.5: 
+                self.pushForwardTime = 3
+                self.backUpTime = 2
+                self.robotState = RobotState.PushTarget
+
+    def pushBehavior(self):
+        self.linearVelocity = 0.4
+
+        self.pushForwardTime -= 1 / self.sampleRate
+
+        if self.pushForwardTime <= 0:   
+            self.robotState = RobotState.BackUpFromTarget
+
+    def backUpBehavior(self):
+        self.linearVelocity = -0.4
+
+        self.backUpTime -= 1 / self.sampleRate
+
+        if self.backUpTime <= 0: 
+           self.robotState = RobotState.LookForTarget
+
+    def driveRobot(self):
         # Publish any inputs provided by the bot.
         self.twist = Twist()
         self.twist.angular.z = self.angularVelocity
         self.twist.linear.x = self.linearVelocity
         self.publishTwist.publish(self.twist)
 
-    def findTargetObject(self):
-        currentContour = 0
-
-        # Sort by area. The biggest (Closest ones) should take priority
-        self.contours = sorted(self.contours, key=cv2.contourArea, reverse=True)
-
-        # If we can see a contour, rotate towards it
-        for contour in self.contours:
-            currentContour += 1
-            M = cv2.moments(contour)
-            if M['m00'] <= 0:
-                continue
-            
-            # Get the Height of the contour center
-            cy = int(M['m01']/M['m00'])
-
-            # If the object is higher than our robot. Its probably out of reach. Ignore it.
-            if cy <= self.cameraData.height / 2:
-                continue
-
-            self.calculateObjectDistance(contour)
-            self.calculateAngleToObject(M)
-
-            # Determine the laser segment we want to get our wall distance reading from.
-            # It should be in the direction of the cube we are targeting.
-            # We can then get a distance between the object and the wall.
-            laserToCheck = self.laserData.ranges[int(len(self.laserData.ranges) / 2) - self.laserAngleDeg]
-            objectToWallDistance = laserToCheck - self.distanceFromTarget
-
-            # Check if the distance between the object and the wall is less than the specified
-            # amount. If it is. We have already pushed that to the wall. Ignore it!
-            if objectToWallDistance < 0.4:
-                #print("Object is already at a wall.", objectToWallDistance)
-                continue
-
-            # Changes the colour of the target laser in RVIZ2. Used for debug purposes.
-            self.laserData.intensities[int(len(self.laserData.ranges) / 2) - self.laserAngleDeg] = 100
-            self.publishLaserScan.publish(self.laserData)
-
-            # If we got here, We want to head to this cube.
-            self.targetObject = contour
-            #print("Found target Object")
-            break
-
-    def headToTarget(self):
-        # Get the X position of the contour
-        M = cv2.moments(self.targetObject)
-        cx = int(M['m10']/M['m00'])
-
-        if self.distanceFromTarget > 0.5: # Move Towards our target
-            self.linearVelocity = 0.2
-
-        print("Target = ", self.cameraData.width / 2)
-        print("Current = ", cx)
-
-        # If the object's center is to the left, turn left.
-        if cx < self.cameraData.width / 2.1:
-            self.angularVelocity = 0.2
-        # If the object's center is to the right, turn right.
-        elif cx > 2 * self.cameraData.width / 2.1 - 100:
-            self.angularVelocity = -0.2
-        # The object is in the center of our camera's view. Switch to the push state.
-        elif self.distanceFromTarget <= 0.6: 
-            self.robotState = RobotState.PushTarget
-
-    def calculateAngleToObject(self, M):
-        # Get the X position of the contour
-        cx = int(M['m10']/M['m00'])
-
-        deltaX = cx - self.cameraData.width / 2
-        laserAngleRad = np.arctan2(deltaX, self.cameraData.width / 2)
-        self.laserAngleDeg = int(np.degrees(laserAngleRad))
-
-    def calculateObjectDistance(self, contour):
-        # Get the bounding box of this contour.
-        # Then get the width of it. We can use this to calculate the distance.
-        # https://www.geeksforgeeks.org/realtime-distance-estimation-using-opencv-python/
-        boundingBox = cv2.minAreaRect(contour)
-        apparentWidth = boundingBox[1][0]
-        self.distanceFromTarget = (self.cubeSize * self.cameraData.width / apparentWidth)
+        # Refresh the robots velocity after since it will receive instructions every tick.
+        self.angularVelocity = 0.0
+        self.linearVelocity = 0.0
 
 def main(args = None):
 
@@ -225,7 +251,7 @@ def main(args = None):
     rclpy.init(args = args)
 
     # Update 10 times per second.
-    sampleRate = 10
+    sampleRate = 30
 
     # Create Instance of our TidyScene Program.
     tidyScene = TidyScene(sampleRate)
